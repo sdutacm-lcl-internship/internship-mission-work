9ba5a8b1007056eefc9158758140514524ab53db
snow <550129432@qq.com>
2023-07-17 19:02:43 +0800

    works/MyOnlySnow/M1.1

--- works/MyOnlySnow/M1.1/main.py
+++ works/MyOnlySnow/M1.1/main.py
@@ -3,7 +3,7 @@ import urllib.request
 from lxml import etree
 import json
 
-def bark(handle):
+def resolve(handle):
     url = "https://codeforces.com/profile/{}/".format(handle)
     headers = {
         'user-agent': ' Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36'
@@ -16,11 +16,12 @@ def bark(handle):
         now = tree.xpath("//div[@class='info']/ul[1]/li[1]/span[1]/text()")
         max = tree.xpath("//div[@class='info']/ul[1]/li[1]//span[@class='smaller']/span[2]/text()")
         contribution = tree.xpath("//div[@class='info']//ul/li[2]/span/text()")
-        woof = tree.xpath("//div[@class='user-rank']//span[@class='user-legendary']/text()")
+        Rank = tree.xpath("//div[@class='user-rank']//span[@class='user-legendary']/text()")
         if contribution == []:
             contribution = tree.xpath("//div[@class='info']//li[1]/span")
             if contribution == []:
-                sys.stderr.write("No such handle\n你在狗叫什么呀\n")
+                sys.stderr.write("No such handle\n")
+                return 1
         if  not now or max ==[]:
             data ={
                 "name":handle
@@ -31,7 +32,7 @@ def bark(handle):
             data ={
                 "name":handle,
                 "rating":int(now[0]),
-                "rank":woof[0][:-1]
+                "rank":Rank[0][:-1]
             }
             kun = json.dumps(data)
             sys.stdout.write(kun+"\n")
@@ -40,8 +41,8 @@ def bark(handle):
     return
 
 def main():
-    dog = sys.argv[1:]
-    for handle in dog:
-        bark(handle.strip())
+    elements = sys.argv[1:]
+    for handle in elements:
+        resolve(handle.strip())
 if __name__ == '__main__':
     main()
