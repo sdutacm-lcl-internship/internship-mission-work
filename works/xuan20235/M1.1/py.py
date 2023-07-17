4e3ff39cb807ef96b2104aec615f93224e4940d2
unknown <1635428720@qq.com>
2023-07-17 10:09:36 +0800

    update

--- works/xuan20235/M1.1/py.py
+++ works/xuan20235/M1.1/py.py
@@ -5,38 +5,43 @@ import re
 from bs4 import BeautifulSoup
 import lxml
 import sys
+#import chaojiying
+
 headers={
  "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
 url_base="https://codeforces.com/profile/"
-handle=input("输入要查询的用户名")
+#handle=input("输入要查询的用户名")
 #handle="aaabbbbccc00"
-url=url_base+handle
-response=requests.get(url=url,headers=headers)
-#print(response.status_code)
-if response.status_code==200 :
-    page_text=response.text
-    #print(page_text)
-    soup=BeautifulSoup(page_text,"lxml")
-    a=soup.select(".info > ul > li >span")
-    if len(a)!=0:
-        rate=a[0].string
-        
-        if rate!="0":
-            b=soup.select(".info > div > div > span")
-            rank=b[0].string
-            date={
-            "handle": handle,
-            "rating": rate,
-            "rank": rank
-            }
-            res_json=json.dumps(date)
-            sys.stdout.write(res_json + '\n')
+args=sys.argv[1:]
+for arg in args :
+    handle=arg
+    url=url_base+handle
+    response=requests.get(url=url,headers=headers)
+    #print(response.status_code)
+    if response.status_code==200 :
+        page_text=response.text
+        #print(page_text)
+        soup=BeautifulSoup(page_text,"lxml")
+        a=soup.select(".info > ul > li >span")
+        if len(a)!=0:
+            rate=a[0].string
+            
+            if rate!="0":
+                b=soup.select(".info > div > div > span")
+                rank=b[0].string
+                date={
+                "handle": handle,
+                "rating": rate,
+                "rank": rank
+                }
+                res_json=json.dumps(date)
+                sys.stdout.write(res_json + '\n')
+            else :
+                date={
+                    "handle":handle
+                }
+                res_json=json.dumps(date)
+                sys.stdout.write(res_json + '\n')
         else :
-            date={
-                "handle":handle
-            }
-            res_json=json.dumps(date)
-            sys.stdout.write(res_json + '\n')
-    else :
-        sys.stderr.write("no such handle\n")
+            sys.stderr.write("no such handle\n")
