b4212e4d08fd85a32513222600486a817060d34c
gcy <2648454867@qq.com>
2023-07-17 21:26:50 +0800

    updata

--- works/guoChengYin/M1.2/main.py
+++ works/guoChengYin/M1.2/main.py
@@ -3,11 +3,9 @@ import sys
 import requests
 
 
-def solve(usernames):
-  #拼接字符串,并以;分割
-  usernames = ';'.join(usernames)
-
-  url = 'https://codeforces.com/api/user.info?handles='+usernames
+def solve(username):
+  #单个用户
+  url = 'https://codeforces.com/api/user.info?handles='+username
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/57.36'
   }
@@ -15,47 +13,51 @@ def solve(usernames):
   status_code = response.status_code
   if status_code==200:
     # 使用json的loads方法将字符串对象转化为pyhon对象
-    res = json.loads(response.text)
-    # 结果列表user_list
-    user_list = res['result']
+    res_py = json.loads(response.text)
+    # 用户数据user_data
+    user_data = res_py['result'][0]
 
-    # 从结果列表中取出数据并封装
-    data = []
-    for item in user_list:
-      user_dic=dict()
-      user_dic["handle"]=str(item['handle']).strip()
-      #使用dict.get(key,defaultValue)方法，避免出现keyError
-      if not item.get("rating",-1)==-1:
-       user_dic["rating"]=int(item['rating'])
-      if not item.get("rank",-1)==-1:
-       user_dic["rank"] = str(item['rank']).strip()
-      data.append(user_dic)
-    json_data=json.dumps(data)
+    #创建空字典用来保存提取的用户数据
+    user_dic = dict()
+    #返回码为200，handle存在,不用担心keyError
+    user_dic["handle"]=str(user_data["handle"]).strip()
+    #对于rating为空的情况，user_dict不会添加rating信息，保证了数据格式正确
+    if not user_data.get("rating",-1)==-1:
+      user_dic["rating"]=int(user_data["rating"])
+    if not user_data.get("rank",-1)==-1:
+      user_dic["rank"]=str(user_data["rank"]).strip()
+
+    #封装
+    json_data=json.dumps(user_data)
     sys.stdout.write(json_data+"\n")
-    return 0
-  #异常情况1 400，”不存在的域名“ 认为没有该选手的信息
   elif status_code==400:
-    sys.stderr.write('no such handle' + "\n"+'ErrorCode '+str(status_code)+"\n")
+    sys.stderr.write('您要寻找的用户不存在\n错误代码'+str(status_code)+"\n")
     return 1
-  #异常情况2 访问被拒绝403
   elif status_code==403:
-    sys.stderr.write('request was denied'+"\n"+"ErrorCode"+str(status_code)+"\n")
+    sys.stderr.write("您的请求被拒绝，可增加动态headers或者使用代理ip\n错误代码"+str(status_code)+"\n")
     return 1
-  #其他异常码
+  elif status_code==404:
+    sys.stderr.write("找不到资源,可重新检查url和headers\n错误代码"+str(status_code)+"\n")
+  elif status_code==503:
+    sys.stderr.write("服务器过载,可调用time.sleep(s)或减少线程数目降低爬取频率\n错误代码"+str(status_code)+"\n")
   else:
-    sys.stderr.write('other errors' + "\n" + "ErrorCode" + str(status_code) + "\n")
-    return 1
+    sys.stderr.write("其他错误类型\n错误代码"+str(status_code)+"\n")
 
 
 if __name__ == '__main__':
-
   args=sys.argv[1:]
+  return_code=0
   #异常情况3  防止空输入
   if len(args)==0:
-    returnCode=1
+    return_code=1
     sys.stderr.write("The input cannot be empty"+"\n")
   else:
-    returnCode=solve(args)
-  exit(returnCode)
+    for username in args:
+      return_code=solve(username)
+      #如果发现错误信息，立即退出程序
+      if return_code==1:
+        exit(return_code)
+  #正常退出程序
+  exit(return_code)
 
 
