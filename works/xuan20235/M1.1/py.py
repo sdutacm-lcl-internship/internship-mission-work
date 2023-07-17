d63d49ca4c22b1381f35629b20d8751939adc8dd
unknown <1635428720@qq.com>
2023-07-17 11:13:40 +0800

    upadta again

--- works/xuan20235/M1.1/py.py
+++ works/xuan20235/M1.1/py.py
@@ -6,27 +6,19 @@ from bs4 import BeautifulSoup
 import lxml
 import sys
 #import chaojiying
-
-headers={
- "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-}
-url_base="https://codeforces.com/profile/"
-#handle=input("输入要查询的用户名")
-#handle="aaabbbbccc00"
-args=sys.argv[1:]
-for arg in args :
-    handle=arg
+def func(handle):
+    headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+    url_base="https://codeforces.com/profile/"
     url=url_base+handle
     response=requests.get(url=url,headers=headers)
-    #print(response.status_code)
     if response.status_code==200 :
         page_text=response.text
-        #print(page_text)
         soup=BeautifulSoup(page_text,"lxml")
         a=soup.select(".info > ul > li >span")
         if len(a)!=0:
             rate=a[0].string
-            
             if rate!="0":
                 b=soup.select(".info > div > div > span")
                 rank=b[0].string
@@ -45,4 +37,11 @@ for arg in args :
         else :
             sys.stderr.write("no such handle\n")
             sys.exit(1) 
-print("over!")
+def main():
+   
+    args=sys.argv[1:]
+    for arg in args :
+        func(arg)
+
+if __name__ == "__main__":
+    main()
