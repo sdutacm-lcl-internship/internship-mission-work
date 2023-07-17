bf06b3fa4b458f3eac7b5de79a6f2f19324acb74
unknown <1635428720@qq.com>
2023-07-17 21:47:55 +0800

    尝试提交

--- works/xuan20235/M1.2/py.py
+++ works/xuan20235/M1.2/py.py
@@ -1,4 +1,5 @@
 from http.client import responses
+from netrc import netrc
 import requests
 import json
 import os
@@ -23,12 +24,12 @@ def func(handle):
     #print(response.status_code)
     if response.status_code != 200 and response.status_code != 400:  #1 解决url 错误 400是没找到通过下面的来处理
         sys.stderr.write(f"status_code ={response.status_code}\n")
-        return 2
+        return 0
     load_json = json.loads(response.text)
-    #print(load_json)
+
     if load_json["status"] == 'FAILED':
         sys.stderr.write("no such handle\n")
-        #print(1)
+
         return 1
     else:
 
@@ -36,44 +37,44 @@ def func(handle):
 
         result = load_json["result"]
 
-        try:
-            rate = result[0]["rating"]
-            rank = ""
-            for tmp in result[0]["rank"]:
-                rank = rank + tmp
+        if 'rating' in result[0]:
 
-            ans = {"handle": handle, "rate": rate, "rank": rank.strip()}
+            rate = result[0]["rating"]
+            rank = result[0]["rank"]
+            #for tmp in result[0]["rank"]:
+            #    rank = rank + tmp
+
+            ans = {"handle": handle, "rating": rate, "rank": rank.strip()}
             res_json = json.dumps(ans)
             sys.stdout.write(res_json + '\n')
-        except:
+        else:
             ans = {"handle": handle}
             res_json = json.dumps(ans)
             sys.stdout.write(res_json + '\n')
+    return 0
 
 
 def main():
 
     args = sys.argv[1:]
+    network_f = 0
+    exit_f = 0
     for arg in args:
-        f = 1
-        exit_f = 0
-        for i in range(1, 10):  # 2解决网络不佳 自动重连
+
+        for a in range(1, 10):  # 2解决网络不佳 自动重连
             try:
-                if func(arg) == 1:
-                    exit_f = 1
-                    f = 0
-                    break
-                f = 0
+                exit_f = func(arg)
+                network_f = 1
                 break
             except:
                 continue
             time.sleep(1)  #3 防止访问太多被拉黑了
-        if f == 1:
-            sys.stderr.write("无法链接到网络\n")
+
         if exit_f == 1:
             sys.exit(1)  #无法找到用户时 exit 1
+        if network_f == 0:
+            sys.stderr.write("无法链接到网络\n")
 
 
 if __name__ == "__main__":
     main()
-
