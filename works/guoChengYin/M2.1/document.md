f127f16f84cfd445df2f77616a55a0cb3f87145a
gcy <2648454867@qq.com>
2023-07-18 16:43:17 +0800

    包括修改M1.2和新写的M2.1

---
+++ works/guoChengYin/M2.1/document.md
@@ -0,0 +1,26 @@
+**页面输入**
+
+jiangly,zxw,aaabbbccc0,ahz,nnnnnn123
+
+![image-20230718163953901](C:\Users\26484\AppData\Roaming\Typora\typora-user-images\image-20230718163953901.png)
+
+
+
+
+
+**运行结果**
+
+响应头
+
+
+
+<img src="C:\Users\26484\AppData\Roaming\Typora\typora-user-images\image-20230718162506468.png" alt="image-20230718162506468" style="zoom:50%;" />
+
+响应体
+
+<img src="C:\Users\26484\AppData\Roaming\Typora\typora-user-images\image-20230718163003503.png" alt="image-20230718163003503" style="zoom:50%;" />
+
+页面结果
+
+![image-20230718163130815](C:\Users\26484\AppData\Roaming\Typora\typora-user-images\image-20230718163130815.png)
+
--- works/guoChengYin/M2.1/myapp.py
+++ works/guoChengYin/M2.1/myapp.py
@@ -4,7 +4,29 @@ import requests
 from flask import Flask, jsonify
 from flask import render_template  # 渲染
 from flask import request
+
 app = Flask(__name__)
+
+
+# 全局异常捕获，包括程序运行时异常  和  在查询此项时未收到有效 HTTP 响应
+@app.errorhandler(Exception)
+def framework_error(e):
+  # 情况 4：在查询此项时未收到有效 HTTP 响应
+  # 断网状态下报exceptions.ConnectionError
+  if isinstance(e, requests.exceptions.ConnectionError):
+    response = {
+      "success": False,
+      "type": 3,
+      "message": "Request timeout"
+    }
+  else:
+    # 情况 5：在查询此项时程序发生运行时异常
+    response = {
+      "success": False,
+      "type": 4,
+      "message": 'Internal Server Error'
+    }
+  return jsonify(response), 200
 
 
 @app.route('/')  # 初始化加载主页
@@ -12,28 +34,29 @@ def init():
   return render_template('index.html')
 
 
-@app.route('/query',methods=["GET","POST"])
+@app.route('/query', methods=["GET", "POST"])
 def query():
-  #get方法和post方法获取前端数据的方式不同
-  val=request.args.get("val")
+  # get方法和post方法获取前端数据的方式不同
+  val = request.args.get("val")
   name_list = str(val).split(",")
-  #接收序列化结果
-  res_list=solve(name_list)
+  # 接收序列化结果
+  res_list = solve(name_list)
   return jsonify(res_list)
+
 
 def solve(name_list):
   url = 'https://codeforces.com/api/user.info?handles='
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/57.36'
   }
-
-  #结果收集列表
-  res_list=[]
+  # 结果收集列表
+  res_list = []
   # 逐个姓名查询，避免由于单个用户异常导致所有查询失败
   for user_name in name_list:
     user_dict = dict()
     result_dict = dict()
     response = requests.get(url=url + user_name, headers=headers)
+    print(response)
     # 情况1此handle可以查询到
     if response.status_code == 200:
       rep_json = json.loads(response.text)
@@ -45,20 +68,21 @@ def solve(name_list):
         result_dict["handle"] = str(rep_info["handle"])
         if not rep_info.get("rating", -1) == -1:
           result_dict["rating"] = int(rep_info["rating"])
-        if not user_dict.get("rank", -1) == -1:
+        if not rep_info.get("rank", -1) == -1:
           result_dict["rank"] = str(rep_info["rank"]).strip()
         user_dict["result"] = result_dict
         res_list.append(user_dict)
-      # 情况2此handle无法查到
-    elif response.status_code==400:
-        user_dict["success"] = False
-        user_dict["type"] = 1
-        user_dict["message"] = "no such handle"
-        res_list.append(user_dict)
+      # 情况2此handle无法查到 返回400
+    elif response.status_code == 400:
+      user_dict["success"] = False
+      user_dict["type"] = 1
+      user_dict["message"] = "no such handle"
+      res_list.append(user_dict)
+      # 情况3 在查询此项时遭遇异常 HTTP 响应
     else:
       user_dict["success"] = False
       user_dict["type"] = 2
-      user_dict["message"] = "NetworkError"
+      user_dict["message"] = "HTTP response with code "+str(response.status_code)
       details = {
         "status": response.status_code
       }
@@ -67,5 +91,6 @@ def solve(name_list):
   return str(res_list)
 
 
+# 自定义 500 错误处理装饰器，返回自己封装的JSON响应
 if __name__ == '__main__':
   app.run(host='127.0.0.1', debug=True, port=2333)
