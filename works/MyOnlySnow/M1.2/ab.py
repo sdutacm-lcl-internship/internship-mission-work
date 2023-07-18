149c04d60b4de4d6f7a7a7c12b16713307a4a274
snow <550129432@qq.com>
2023-07-18 16:06:13 +0800

    works/MyOnlySnow/M1.2

--- works/MyOnlySnow/M1.2/ab.py
+++ works/MyOnlySnow/M1.2/ab.py
@@ -13,40 +13,47 @@ def resolve(handle):
     headers = {'User-Agent': ua}
     request = urllib.request.Request(url=url, headers=headers)
     try:
-            response = urllib.request.urlopen(request)
-            content = response.read().decode("utf-8")
-            Json = json.loads(content)
-            info_list = Json.get('result', [])
-            info = info_list[0]
-            handle = info.get('handle', '')
-            rating = info.get('rating', 0)
-            rank = info.get('rank', '')
-            max = info.get('maxRating', '')
-            if not rank:
-                data = {
-                    'name': handle,
-                    'rating': rating,
-                    'rank': 'None'
-                }
-            if not rating or max == []:
-                data = {
-                    'name': handle
-                }
-            else:
-                data = {
-                    'name': handle,
-                    'rating': rating,
-                    'rank': rank
-                }
-            print(json.dumps(data) + '\n')
+        response = urllib.request.urlopen(request)
+        content = response.read().decode("utf-8")
+        Json = json.loads(content)
+        info_list = Json.get('result', [])
+        info = info_list[0]
+        handle = info.get('handle', '')
+        rating = info.get('rating', 0)
+        rank = info.get('rank', '')
+        max = info.get('maxRating', '')
+        if not rank:
+            data = {
+                'name': handle,
+                'rating': rating,
+                'rank': 'None'
+            }
+        if not rating or max == []:
+            data = {
+                'name': handle
+            }
+        else:
+            data = {
+                'name': handle,
+                'rating': rating,
+                'rank': rank
+            }
+        print(json.dumps(data) + '\n')
     except urllib.error.HTTPError as error:
         if error.code == 400:
             print("No Such Handle\n")
-            exit(1)
+        elif error.code == 403:
+            sys.stderr.write("Access Forbidden: {}".format(error.code) + '\n')
+        elif error.code == 503:
+            sys.stderr.write("Service Unavailable: {}".format(error.code) + '\n')
+        elif error.code == 404:
+            sys.stderr.write("Not Found: {}".format(error.code) + '\n')
         else:
-            print("Network Error: {}".format(error.code)+'\n')
-            exit(1)
-
+            sys.stderr.write("Network Error: {}".format(error.code) + '\n')
+        exit(1)
+    except urllib.error.URLError as e:
+        sys.stderr.write("URL Error: {}\n".format(e.reason))
+        exit(1)
 
 
 def main():
