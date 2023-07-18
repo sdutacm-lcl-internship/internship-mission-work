2e3445a7f82a4257b60dae7b0ca8c6650a070df6
snow <550129432@qq.com>
2023-07-18 09:32:23 +0800

    works/MyOnlySnow/M1.1

--- works/MyOnlySnow/M1.1/main.py
+++ works/MyOnlySnow/M1.1/main.py
@@ -3,12 +3,11 @@ import urllib.request
 from lxml import etree
 import json
 
+
 def resolve(handle):
     url = "https://codeforces.com/profile/{}/".format(handle)
-    headers = {
-        'user-agent': ' Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36'
-    }
-    request = urllib.request.Request(url=url,headers=headers)
+    headers = {'user-agent': ' Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36'}
+    request = urllib.request.Request(url=url, headers=headers)
     response = urllib.request.urlopen(request)
     if response.code == 200:
         content = response.read().decode('utf-8')
@@ -16,25 +15,23 @@ def resolve(handle):
         now = tree.xpath("//div[@class='info']/ul[1]/li[1]/span[1]/text()")
         max = tree.xpath("//div[@class='info']/ul[1]/li[1]//span[@class='smaller']/span[2]/text()")
         contribution = tree.xpath("//div[@class='info']//ul/li[2]/span/text()")
-        rank = tree.xpath("//div[@id='body']//div[@id='pageContent']//div[@class='userbox']//div[@class='info']//div[@class='user-rank']/span/text()")
+        rank = tree.xpath( "//div[@id='body']//div[@id='pageContent']//div[@class='userbox']//div[@class='info']//div[@class='user-rank']/span/text()")
         if contribution == []:
             contribution = tree.xpath("//div[@class='info']//li[1]/span/text()")
             if contribution == []:
                 sys.stderr.write("No such handle\n")
                 sys.exit(1)
 
-        if  not now or max ==[]:
-            data ={
-                "name":handle
+        if not now or max == []:
+            data = {
+                "name": handle
             }
-            kun = json.dumps(data)
-            sys.stdout.write(kun + "\n")
         else:
             if rank:
-                data ={
-                "name":handle,
-                "rating":int(now[0]),
-                "rank":rank[0][:-1]
+                data = {
+                    "name": handle,
+                    "rating": int(now[0]),
+                    "rank": rank[0][:-1]
                 }
             else:
                 data = {
@@ -42,15 +39,18 @@ def resolve(handle):
                     "rating": int(now[0]),
                     "rank": "None"
                 }
-            kun = json.dumps(data)
-            sys.stdout.write(kun+"\n")
+        real_data = json.dumps(data)
+        sys.stdout.write(real_data + "\n")
     else:
-        sys.stderr.write("Network error"+str(response.code+"\n"))
+        sys.stderr.write("Network error" + str(response.code + "\n"))
     return
+
 
 def main():
     elements = sys.argv[1:]
     for handle in elements:
         resolve(handle.strip())
+
+
 if __name__ == '__main__':
     main()
