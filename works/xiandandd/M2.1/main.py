59f6a68604b89a636800b5d075ee6acc3c3dea35
xiandandd <135024508+xiandandd@users.noreply.github.com>
2023-07-19 09:53:50 +0800

    M2.1 forth commit

--- works/xiandandd/M2.1/main.py
+++ works/xiandandd/M2.1/main.py
@@ -14,14 +14,14 @@ def solve(username):
     }
 
     try:
-        #1/0
-        #新建一个空的response对象，以便于异常的判断
-        response=requests.Response()
+        # 1/0
+        # 新建一个空的response对象，以便于异常的判断
+        response = requests.Response()
         response = requests.get(url, params=params)
         number = response.status_code
         print(number)
         data = response.json()
-        #没有收到响应的情况
+        # 没有收到响应的情况
         if response is None:
             user_info = {
                 "success": False,
@@ -29,7 +29,7 @@ def solve(username):
                 "message": "No valid HTTP response was received when querying this item",
             }
             return user_info
-        #遇到异常http响应的情况,408是Request Timeout，504是Gateway Timeout
+        # 遇到异常http响应的情况,408是Request Timeout，504是Gateway Timeout
         elif number == 404 or number == 503 or number == 403 or number == 500 or number == 408 or number == 504:
             user_info = {
                 "success": False,
@@ -76,10 +76,11 @@ def solve(username):
                 "message": 'no such handle'
             }
             return user_info
-    #发现如果访问api失败也会收到状态码，这种情况下一般是503的状况
-    #访问失败，没有收到状态码就是断网的情况了
+    # 发现如果访问api失败也会收到状态码，这种情况下一般是503的状况
+    # 访问失败，没有收到状态码就是断网的情况了
     except requests.exceptions.RequestException as e:
-        if response is not None and response.status_code != 200 and response.status_code !=400:
+        # print(response)
+        if response.status_code != None and response.status_code != 200 and response.status_code != 400:
             user_info = {
                 "success": False,
                 "type": 2,
@@ -88,7 +89,7 @@ def solve(username):
                     "status": response.status_code,
                 }
             }
-        #断网了，没有收到响应
+        # 断网了，没有收到响应
         else:
             user_info = {
                 "success": False,
@@ -96,12 +97,12 @@ def solve(username):
                 "message": "No valid HTTP response was received when querying this item",
             }
         return user_info
-    #这是程序异常的情况
+    # 这是程序异常的情况
     except:
-        user_info={
-            "success":False,
-            "type":4,
-            "message":"Internal Server Error"
+        user_info = {
+            "success": False,
+            "type": 4,
+            "message": "Internal Server Error"
         }
         return user_info
 
@@ -121,8 +122,6 @@ def query_handles():
     # jsonify()函数简化了将数据转换为JSON响应的过程，并确保响应的Content-Type标头正确设置为application/json
     # 这样，浏览器或其他客户端会正确地解析返回的JSON数据了
     return jsonify(response_data)
-
-
 
 
 if __name__ == '__main__':
