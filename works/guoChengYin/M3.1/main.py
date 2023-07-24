89cc901386e529103481e746d8d9e3facf3a7c75
gcy <2648454867@qq.com>
2023-07-24 20:27:43 +0800

    格式化一下代码

--- works/guoChengYin/M3.1/main.py
+++ works/guoChengYin/M3.1/main.py
@@ -12,13 +12,15 @@ from flask_caching import Cache
 from my_utils.utils import Crawler
 
 app = Flask(__name__)
-#两个缓存器 1放
+# 两个缓存器 1放
 cache_user_info = Cache(app, config={'CACHE_TYPE': 'simple'})
 cache_user_ratings = Cache(app, config={'CACHE_TYPE': 'simple'})
-#自定义爬虫
+# 自定义爬虫
 crawler = Crawler()
 
 myUtils = Utils()
+
+
 # 捕捉其他不可预知的错误，用一个全局异常处理器处理
 @app.errorhandler(Exception)
 def server_error(e):
@@ -41,14 +43,12 @@ def batch_get_user_info():
         response_data.append(request_results)
         continue
 
-
       # 发起请求,并准备一个空的字典
       request_info = crawler.crawl('https://codeforces.com/api/user.info?handles={}'.format(name))
-      #不能用clear()此时该变量还在指向response_data中的数据
-      request_results={}
+      # 不能用clear()此时该变量还在指向response_data中的数据
+      request_results = {}
 
-
-      #返回值为400的情况
+      # 返回值为400的情况
       if request_info['status'] == 400:
         request_results['success'] = False
         request_results['type'] = 1
@@ -56,8 +56,7 @@ def batch_get_user_info():
         response_data.append(request_results)
         continue
 
-
-      #返回值不等于400并且不等于200
+      # 返回值不等于400并且不等于200
       if request_info['status'] != 200:
         request_results['success'] = False
         request_results['type'] = 2
@@ -69,19 +68,18 @@ def batch_get_user_info():
         response_data.append(request_results)
         continue
 
-
-      #剩下的就是返回值为200的情况
+      # 剩下的就是返回值为200的情况
       result = request_info['result'][0]
       # 有rating的情况
       if 'rating' in result:
-        request_results={
+        request_results = {
 
-          "success":True,
-          "result":{
-          "handle": result['handle'],
-          "rating": int(result['rating']),
-          "rank": result['rank']
-        }
+          "success": True,
+          "result": {
+            "handle": result['handle'],
+            "rating": int(result['rating']),
+            "rank": result['rank']
+          }
         }
       else:
         # 无rating的情况
@@ -93,7 +91,7 @@ def batch_get_user_info():
       response_data.append(request_results)
       print(response_data)
       cache_user_info.set(name, request_results, timeout=30)
-      #存入文件
+      # 存入文件
       myUtils.data_save('data-user-info.txt', {"handle": name, "info": request_results})
     except Exception as e:
       # 第一种异常，请求异常，未收到有效响应
@@ -112,19 +110,7 @@ def batch_get_user_info():
   return jsonify(response_data), 200
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#路由2 比赛信息
+# 路由2 比赛信息
 @app.route('/getUserRatings')
 def get_user_ratings():
   try:
@@ -146,7 +132,7 @@ def get_user_ratings():
     # 3.请求后返回码不是400也不是200
     if request_results['status'] != 200:
       error_message = {
-        "message": "An exception HTTP interface response was encountered:{}" .format(request_results['status'])
+        "message": "An exception HTTP interface response was encountered:{}".format(request_results['status'])
       }
 
     # 4.剩下的就是200，取出result。并定义一个搜集结果的列表
@@ -190,22 +176,17 @@ def get_user_ratings():
       raise
 
 
-
-
-#项目启动前将txt文件中保存的数据加载到缓存中
+# 项目启动前将txt文件中保存的数据加载到缓存中
 def load_cache():
-  user_info=myUtils.read_file('data-user-info.txt')
-  user_ratings=myUtils.read_file('data-user-ratings.txt')
+  user_info = myUtils.read_file('data-user-info.txt')
+  user_ratings = myUtils.read_file('data-user-ratings.txt')
   for name in user_info.keys():
     cache_user_info.set(name, user_info[name], timeout=30)
-  for name in  user_ratings.keys():
+  for name in user_ratings.keys():
     cache_user_ratings.set(name, user_ratings[name], timeout=30)
 
 
-
-
-
 if __name__ == '__main__':
-  #项目启动前先将文件中的数据填充到缓存中
+  # 项目启动前先将文件中的数据填充到缓存中
   load_cache()
   app.run(host='127.0.0.1', port=2333)
