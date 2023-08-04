8e67e6800efe0bb5634853700ec3103970410df2
gcy <2648454867@qq.com>
2023-08-04 16:59:25 +0800

    test

---
+++ works/guoChengYin/M4.1/dao/user_dao.py
@@ -0,0 +1,88 @@
+import datetime
+import sqlite3
+import sys
+import time
+
+
+class Dao:
+  def query_user_info(self, handle):
+    conn = sqlite3.connect('cf.db')
+    # 获取游标对象
+    cursor = conn.cursor()
+    sql = "SELECT * FROM user_info WHERE handle ='{}'".format(handle)
+    cursor.execute(sql)
+
+    res = cursor.fetchall()
+    if len(res) != 0:
+      res = list(res[0])
+      updatated = res[3]
+      diff = self.get_time_diff(updatated)
+      if diff > 30:
+        res = []
+      else:
+        res.append(diff)
+    conn.close()
+    return res
+
+  def save_user_info(self, handle, user_info, update_at):
+    conn = sqlite3.connect('cf.db')
+    # 获取游标对象
+    cursor = conn.cursor()
+    rating = user_info["result"].get("rating", None)
+    rank = user_info["result"].get("rank", None)
+    cursor.execute("delete from user_info where handle = '{}'".format(handle))
+    cursor.execute(
+      "insert into user_info (handle,rating,rank,updated_at) VALUES ('{}','{}','{}','{}')".format(handle, rating,
+                                                                                                  rank, update_at))
+    conn.commit()
+    conn.close()
+
+  def save_ratings(self, ratings, update_time_at):
+    conn = sqlite3.connect('cf.db')
+    cursor = conn.cursor()
+    for rating in ratings:
+      handle = rating["handle"]
+      contest_id = rating["contestId"]
+      contest_name = rating["contestName"]
+      rank = rating["rank"]
+      old_rating = rating["oldRating"]
+      new_rating = rating["newRating"]
+      updated_at = rating["ratingUpdatedAt"]
+      cursor.execute("delete from user_rating where handle = '{}'".format(handle))
+      cursor.execute(
+        "insert into user_rating (handle,contest_id,contest_name,rank,old_rating,new_rating,rating_updated_at,updated_at) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}')".
+        format(handle, contest_id, contest_name, rank, old_rating, new_rating, updated_at, update_time_at))
+    conn.commit()
+    conn.close()
+
+  def query_ratings(self, handle):
+    conn = sqlite3.connect('cf.db')
+    # 获取游标对象
+    cursor = conn.cursor()
+    sql = "SELECT * FROM user_rating WHERE handle='{}'".format(handle)
+
+    cursor.execute(sql)
+    res = cursor.fetchall()
+
+    result = []
+    if len(res) != 0:
+      diff = self.get_time_diff(res[0][8])
+
+      if diff < 30:
+        for item in res:
+          result.append({
+            "handle": item[1],
+            "contestId": item[2],
+            "contestName": item[3],
+            "rank": item[4],
+            "ratingUpdatedAt": item[7],
+            "oldRating": item[5],
+            "newRating": item[6]
+          })
+        result.append(diff)
+    conn.close()
+    return result
+
+  @classmethod
+  def get_time_diff(self, dt):
+    return round(time.time()) - dt
---
+++ works/guoChengYin/M4.1/myapp.py
@@ -0,0 +1,83 @@
+import json
+import pickle
+
+from flask_cors import CORS
+from flask_cors import cross_origin
+from flask import Flask, request, jsonify, render_template
+from flask_caching import Cache
+from service.service import Service
+from config import Config
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+cache_user_info = Cache(app, config={'CACHE_TYPE': 'simple'})
+cache_user_ratings = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+service = Service(cache_user_info, cache_user_ratings)
+
+
+# @app.errorhandler(Exception)
+# def server_error(e):
+#   error_message = {"message": 'Internal Server Error'}
+#   return jsonify(error_message), 500
+
+
+@app.route('/getUserRatings')
+def get_user_ratings():
+  handle = request.args.get('handle')
+  handle = handle.replace('{', '').replace('}', '')
+  res = service.get_user_ratings(handle)
+  return jsonify(res[0]), res[1]
+
+
+@app.route('/batchGetUserInfo')
+def get_user_info():
+  handles = request.args.get('handles')
+  handles = handles.replace('{', '').replace('}', '').split(',')
+  user_infos = service.batch_get_user_info(handles)
+  return jsonify(user_infos)
+
+
+@app.route('/')
+def show_index():
+  return render_template("index.html")
+
+
+@app.route('/requestUserInfo', methods=["GET", "POST"])
+@cross_origin(supports_credentials=True)
+def request_use_info():
+  handle = request.args.get("handle")
+  handle = str(handle).strip()
+  response_data = {}
+  response_data["handle"] = handle
+  handles = []
+  handles.append(handle)
+  info_res = service.batch_get_user_info(handles)[0]
+  if info_res.get("success") == False:
+    return jsonify(info_res["message"]), 400
+  response_data["rating"] = "暂无"
+  if "result" in info_res.keys():
+    response_data["rating"] = info_res["result"].get("rating", "暂无")
+  info_res = service.get_user_ratings(handle)
+  if info_res[1] != 200:
+    return jsonify(info_res[0]["message"]), info_res[1]
+
+  info_res = info_res[0]
+  rating_history = []
+  for item in info_res:
+    rating = {}
+    if not isinstance(item, dict):
+      continue
+    rating["contestName"] = item["contestName"]
+    rating["ratingUpdatedAt"] = item["ratingUpdatedAt"]
+    rating["rank"] = item["rank"]
+    rating["oldRating"] = item["oldRating"]
+    rating["newRating"] = item["newRating"]
+    rating_history.append(rating)
+  response_data["rating_history"] = rating_history
+  response_data["success"] = True
+  return jsonify(response_data)
+
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=2333)
