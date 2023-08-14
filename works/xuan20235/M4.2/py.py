317269893e3a2066836676b79efe0a0642b724c8
unknown <1635428720@qq.com>
2023-08-14 20:30:57 +0800

    update

---
+++ works/xuan20235/M4.2/py.py
@@ -0,0 +1,109 @@
+import flask
+import sqlite3
+from flask import Flask, request, Response, make_response
+import requests
+import json
+from datetime import timedelta, datetime
+import pytz
+import query
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['DEBUG'] = True
+
+
+def update_info(handle, rating, rank):
+    with sqlite3.connect('cf.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON")
+        now = datetime.now(pytz.timezone('Asia/Shanghai')).isoformat()
+        cursor.execute(
+            '''
+        INSERT OR REPLACE INTO user_info(handle,rating,rank,updated_at)
+        VALUES(?,?,?,?)
+        ''', (handle, rating, rank, now))
+        conn.commit()
+
+
+def update_rating(handle, contest_id, contest_name, rank, old_rating,
+                  new_rating, ratingUpdatedAt):
+    with sqlite3.connect('cf.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON")
+        now = datetime.now(pytz.timezone('Asia/Shanghai')).isoformat()
+        cursor.execute("SELECT handle FROM user_info WHERE handle = ?",
+                       (handle, ))
+        data = cursor.fetchone()
+        if data is None:
+            query.get_user_info(handle)
+        cursor.execute(
+            '''
+                    INSERT OR REPLACE INTO user_ratings(handle,contest_id,contest_name,rank,old_rating,new_rating,rating_updated_at,updated_at)
+                    VALUES(?,?,?,?,?,?,?,?)
+            ''', (handle, contest_id, contest_name, rank, old_rating,
+                  new_rating, ratingUpdatedAt, now))
+        conn.commit()
+
+
+@app.route('/batchGetUserInfo')
+def URL_handles():
+    handles = request.args.get('handles', '').split(',')
+    results = []
+    for handle in handles:
+        result = query.get_user_info(handle)
+        results.append(result)
+    return Response(json.dumps(results), mimetype='application/json')
+
+
+@app.route('/getUserRatings')
+def URL_ratings():
+    handle = request.args.get('handle', '')
+    results = []
+    results = query.get_user_rating(handle)
+    if 'message' in results and 'code' in results:
+        result = {'message': results['message']}
+        response = make_response(json.dumps(result), results['code'])
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        if 'message' in results :
+              response = make_response(json.dumps(results), 404)
+              response.headers['Content-Type'] = 'application/json'
+              return response
+
+        response = make_response(json.dumps(results), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+def creat_db():
+    with sqlite3.connect('cf.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_info (
+                 handle VARCHAR PRIMARY KEY NOT NULL COLLATE NOCASE,
+                rating INT,
+                rank VARCHAR,
+                updated_at DATETIME NOT NULL
+            )
+        ''')
+        cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS user_ratings(
+                        user_rating_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        handle VARCHAR NOT NULL COLLATE NOCASE,
+                        contest_id INT NOT NULL,
+                        contest_name VARCHAR NOT NULL,
+                        rank INT NOT NULL,
+                        old_rating INT NOT NULL,
+                        new_rating INT NOT NULL,
+                        rating_updated_at DATETIME NOT NULL,
+                        updated_at DATETIME NOT NULL,
+                        FOREIGN KEY (handle) REFERENCES user_info (handle),
+                        UNIQUE(handle, contest_id) ON CONFLICT REPLACE
+                    );
+                ''')
+    conn.commit()
+
+
+if __name__ == '__main__':
+    creat_db()
+    app.run(host='127.0.0.1', port=2333, debug=True)
---
+++ works/xuan20235/M4.2/query.py
@@ -0,0 +1,257 @@
+from multiprocessing.connection import answer_challenge
+import flask
+import sqlite3
+from flask import Flask, request, Response, make_response
+import requests
+import json
+from datetime import timedelta, datetime
+import pytz
+import py
+
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36 Edg/113.0.1774.42'
+}
+
+
+def get_user_info(handle):
+    with sqlite3.connect('cf.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            '''
+        SELECT rating,rank FROM user_info
+        WHERE handle = ? AND updated_at > ?
+        ''', (handle, (datetime.now(pytz.timezone('Asia/Shanghai')) -
+                       timedelta(seconds=30)).isoformat()))
+        row = cursor.fetchone()
+        #print(row)
+        if row:
+            rating, rank = row
+            if rating == None:
+                ans = {
+                    "success": False,
+                    "type": "1",
+                    "message": "no such handle"
+                }
+                return ans
+            elif rating == 0:
+                print("yaunshen")
+                data = {
+                    "handle": handle,
+                }
+            else:
+                data = {
+                    "handle": handle,
+                    "rating": rating,
+                    "rank": rank,
+                }
+            ans = {"success": True, "result": data}
+            return ans
+    ans = func(handle)
+    jd_flag = ans[0]
+    ans = ans[1]
+    #print(jd_flag)
+    if jd_flag == 200:
+        data = {
+            "handle": handle,
+            "rating": ans[0],
+            "rank": ans[1],
+        }
+    elif jd_flag == 203:
+        data = {
+            "handle": handle,
+        }
+    elif jd_flag == 404:
+        ans = {"success": False, "type": "1", "message": "no such handle"}
+        return ans
+    elif jd_flag == 501:
+        ans = {
+            "success": False,
+            "type": '4',
+            "message": "Internal Server Error"
+        }
+        return ans
+    elif jd_flag == 503:
+        ans = {"success": False, "type": '3', "message": "网络异常"}
+        return ans
+    elif jd_flag == 500:
+        return ans
+    ans = {"success": True, "result": data}
+    return ans
+
+
+def func(handle):
+    ans = []
+    url = f"https://codeforces.com/api/user.info?handles={handle}"
+    try:
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        Json = response.json()
+        info_list = Json.get('result', [])
+        info = info_list[0]
+        handle = info.get('handle', '')
+        rating = info.get('rating', '0')
+        rank = info.get('rank', '')
+        py.update_info(handle, rating, rank)
+        ans.append(rating)
+        ans.append(rank)
+        if rating == '0':
+            return 203, ans
+        else:
+            return 200, ans
+    except requests.exceptions.ConnectionError:
+        return 503, ans
+    except requests.exceptions.HTTPError as error:
+        if error.response.status_code == 400:
+            py.update_info(handle, None, None)
+            return 404, ans
+        else:
+            data = {
+                'success': False,
+                'type': 2,
+                'message':
+                f'HTTP response with code {error.response.status_code}',
+                'details': {
+                    'status': error.response.status_code
+                }
+            }
+            return 500, data
+    except Exception:
+        return 501, ans
+
+
+def sovle(handle):
+    with sqlite3.connect('cf.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            '''
+        SELECT rating,rank FROM user_info
+        WHERE handle = ? AND updated_at > ?
+        ''', (handle, (datetime.now(pytz.timezone('Asia/Shanghai')) -
+                       timedelta(seconds=30)).isoformat()))
+        row = cursor.fetchone()
+        if row:
+            rating, rank = row
+            if rating == None:
+                return 1
+            elif rating == 0:
+                return 2
+            else:
+                return 3
+        ans = func(handle)
+        jd_flag = ans[0]
+        print(jd_flag)
+        if jd_flag == 200:
+            return 3
+        elif jd_flag == 203:
+            return 2
+        elif jd_flag == 404:
+            return 1
+        else:
+            return jd_flag
+
+
+def get_user_rating(handle):
+
+    jd_flag = sovle(handle)
+    ans = {}
+    if jd_flag == 1:
+        ans = {"message": "no such handle"}
+        answer = []
+        answer.append(ans)
+        return ans
+    elif jd_flag == 2:
+        answer = []
+        return answer
+
+    elif jd_flag == 3:
+        with sqlite3.connect('cf.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                '''
+            SELECT contest_id,contest_name,rank,old_rating,new_rating,rating_updated_at FROM user_ratings
+            WHERE handle = ? AND updated_at > ?
+            ''', (handle, (datetime.now(pytz.timezone('Asia/Shanghai')) -
+                           timedelta(seconds=30)).isoformat()))
+            rows = cursor.fetchall()
+            if rows:
+                data = []
+                for row in rows:
+                    contest_id, contest_name, rank, old_rating, new_rating, rating_updated_at = row
+                    data.append({
+                        'handle': handle,
+                        'contestId': contest_id,
+                        'contestName': contest_name,
+                        'rank': rank,
+                        'ratingUpdatedAt': rating_updated_at,
+                        'oldRating': old_rating,
+                        'newRating': new_rating
+                    })
+                return data
+            print("yaunshen")
+            ans = func1(handle)
+            return ans
+    else:
+        ans = {"message": '又错了已黑化', "code": jd_flag}
+        return ans
+
+
+def func1(handle):
+    ans = {}
+    url = f"https://codeforces.com/api/user.rating?handle={handle}"
+    try:
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        Json = response.json()
+        info_list = Json.get('result', [])
+        result = []
+        for info in info_list:
+            contestId = info.get('contestId', 0)
+            contestName = info.get('contestName', '')
+            rank = info.get('rank', 0)
+            ratingUpdateTimeSeconds = info.get('ratingUpdateTimeSeconds', 0)
+            oldRating = info.get('oldRating', 0)
+            newRating = info.get('newRating', 0)
+            time = datetime.fromtimestamp(ratingUpdateTimeSeconds,
+                                          pytz.timezone('Asia/Shanghai'))
+            ratingUpdatedAt = time.isoformat()
+            if contestId:
+                py.update_rating(handle, contestId, contestName, rank,
+                                 oldRating, newRating, ratingUpdatedAt)
+            result.append({
+                'handle': handle,
+                'contestId': contestId,
+                'contestName': contestName,
+                'rank': rank,
+                'ratingUpdatedAt': ratingUpdatedAt,
+                'oldRating': oldRating,
+                'newRating': newRating
+            })
+        if result == []:
+            result = {
+                "success": True,
+                "result": {
+                    "handle": handle,
+                }
+            }
+            ans = []
+            ans.append(result)
+            return ans
+        return result
+    except requests.exceptions.HTTPError as error:
+        if error.response.status_code == 400:
+            ans = {'message': 'no such handle', 'code': 404}
+            return ans
+        else:
+            ans = {
+                "success": False,
+                "type": 2,
+                "message":
+                f"HTTP response with code {error.response.status_code}",
+                "details": {
+                    "status": error.response.status_code
+                }
+            }
+            return ans
+    except Exception as error:
+        return {'message': "Internal Server Error", 'code': 500}
