ae20352496e62173e1d8797d21036972e6fb7a24
munchong <845269087@qq.com>
2023-09-14 20:00:01 +0800

    mc-M1.1.2

--- works/munchong/M1.1/munchong-m1.1-1.py
+++ works/munchong/M1.1/munchong-m1.1-1.py
@@ -1,40 +1,53 @@
+# coding=utf-8  
 import sys
 import json
 import requests
 from lxml import html
 
+def main() :
+    # 链接
+    print("please input one nickname:")
+    nickname = sys.stdin.readline().strip()
+    url = "https://codeforces.com/profile/" + nickname
+    response = requests.get(url) 
 
-nickname = input("please input one nickname:") 
-url = "https://codeforces.com/profile/" + nickname
-response = requests.get(url) 
+    # 如果返回成功
+    if response.status_code == 200 :
+        try:
+            # 获取数据 
+            content = html.fromstring(response.content)
+            rating = content.xpath('//div[@class="info"]/ul/li[1]/span/text()')
+            rank = content.xpath('//div[@class="user-rank"]/span/text()')
 
-if response.status_code == 200 :
-    try:
-        content = html.fromstring(response.content)
-        rating = content.xpath('//div[@class="info"]/ul/li[1]/span/text()')
-        rank = content.xpath('//div[@class="user-rank"]/span/text()')
+            #不存在rank时
+            if rank[0] == "Unrated " :
 
-        if rank[0] == "Unrated " :
+                people = {
+                    "handle" : nickname
+                }
 
-            people = {
-                "handle" : nickname
-            }
+                output = json.dumps(people) 
+                print(output, file=sys.stdout)
+                sys.exit(0)
+            elif rating and rank :
+                
+                rank_t = rank[0] 
+                rank_t = rank_t.rstrip(" ")
 
-            output = json.dumps(people) 
-            print(output)
-            sys.exit(0)
-        elif rating and rank :
+                people = {
+                    "handle": nickname ,"rating": rating[0],"rank": rank_t
+                }
 
-            people = {
-                "handle": nickname ,"rating": rating[0],"rank": rank[0]
-            }
-
-            output = json.dumps(people)
-            print(output)
-            sys.exit(0)
-        else:
+                output = json.dumps(people)
+                print(output, file=sys.stdout)
+                sys.exit(0)
+            # 用户不存在时 
+            else:
+                print("no such handle", file=sys.stderr)
+                sys.exit(1)
+        except IndexError:
             print("no such handle", file=sys.stderr)
             sys.exit(1)
-    except IndexError:
-        print("no such handle", file=sys.stderr)
-        sys.exit(1)
+
+
+main() 
