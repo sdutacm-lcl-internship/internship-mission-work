d3fdffebb047c586e59a28c2a138241e4f4804a2
SDUTAIer <sdutwujinhao@gmail.com>
2024-11-15 12:20:09 +0800

    atri second

--- works/atrior/M2/M2.1/main.py
+++ works/atrior/M2/M2.1/main.py
@@ -17,50 +17,79 @@ def get_user_info(username, proxy):
 
     try:
         response = requests.get(API_URL, headers=headers, params=params, proxies=proxies)
-        response.raise_for_status()
+        response.raise_for_status() 
         data = response.json()
-        
+
         if data['status'] == 'OK':
             return extract_user_info(data['result'][0])
         else:
-            return "No such handle"
-    except requests.exceptions.RequestException as e:
-        return "no such handle"
+            return {
+                'success': False,
+                'type': '1;',
+                'message': "no such handle;",
+            }
+    except requests.exceptions.HTTPError as http_error:
+        return {
+            'success': False,
+            'type': '2;',
+            'message': f"HTTP response with code {response.status_code}",
+            'details':{
+                'status':f'{response.status_code};'
+            }
+        }
+    except requests.exceptions.RequestException as req_err:
+        return {
+            'success': False,
+            'type': '3;',
+            'message': "Request timeout"
+        }
+    except RuntimeError as runtime_err:
+        return {
+            'success': False,
+            'type': '4;',
+            'message': "'Internal Server Error';",
+        }
 
 def extract_user_info(user_info):
-    user={}
+    user = {
+        'success': True,
+        'result': {}
+    }
     if 'rank' in user_info:
-        user['handle']=user_info['handle']
-        user['rating']=user_info['rating']
-        user['rank']=user_info['rank']
+        user['result']['handle'] = user_info['handle'] + ';'
+        user['result']['rating'] = str(user_info['rating']) + ';'
+        user['result']['rank'] = user_info['rank'] + ';'
     else:
-        user['handle']=user_info['handle']
+        user['result']['handle'] = user_info['handle'] + ';'
     return user
 
 def get_user_list(usernames):
     user_list = []
     for username in usernames:
         user_list.append(get_user_info(username, PROXY))
-    return user_list
+    format_usr_list=[json.dumps(d, indent=4, ensure_ascii=False) for d in user_list]
+    return format_usr_list
 
 def main(name):
     usernames = name.split(',')
-
     user_info = get_user_list(usernames)
-    
     return user_info
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=='POST':
-        name=request.form.get('name')
-        userinfor=main(name)
-        return render_template('index.html', data=userinfor)
-    else:
-        return render_template('index.html')
-
+    if request.method == 'POST':
+        name = request.form.get('name')
+        user_info = main(name)
+        return render_template('index.html', data=user_info)
+    elif request.method == 'GET':
+        name = request.args.get('handles', '')
+        if name:
+            user_info = main(name)
+            return render_template('index.html', data=user_info)
+        else:
+            return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=2333)
